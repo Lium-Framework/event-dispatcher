4c0b31d8065aa5351bffffec289ba500313b21aa
Florian Hermann <florian.hermann94@gmail.com>
2019-10-28 22:44:07 +0100

    WIP - DefaultListenerProvider tests

--- tests/ListenerProvider/DefaultListenerProviderTest.php
+++ tests/ListenerProvider/DefaultListenerProviderTest.php
@@ -9,6 +9,56 @@ use PHPUnit\Framework\TestCase;
 
 class DefaultListenerProviderTest extends TestCase
 {
+    /** @test */
+    public function MUST_treats_parent_types_identically_to_the_own_type_of_the_Event()
+    {
+        $listeners = [
+            function (A $a) {
+            },
+        ];
+
+        $listenerProvider = new DefaultListenerProvider($listeners);
+
+        $result = $listenerProvider->getListenersForEvent(new B());
+
+        $this->assertSame(
+            $listeners,
+            $result instanceof \Traversable ? iterator_to_array($result) : $result,
+            'The DefaultListenerProvider did not treats parent types identically to the own Event\'t type'
+        );
+    }
+
+    /** @test */
+    public function each_Listeners_MUST_be_type_compatible_with_the_Event()
+    {
+        $listeners = [
+            function (object $object) {
+            },
+            function (A $a) {
+            },
+            function (B $b) {
+            },
+            function (\stdClass $stdClass) {
+            },
+        ];
+
+        $listenerProvider = new DefaultListenerProvider($listeners);
+
+        $event = new B();
+        $result = $listenerProvider->getListenersForEvent($event);
+
+        foreach ($result as $listener) {
+            $closure = \Closure::fromCallable($listener);
+            $reflectionFunction = new \ReflectionFunction($closure);
+            $type = $reflectionFunction->getParameters()[0]->getType()->getName();
+
+            $this->assertTrue(
+                $event instanceof $type || 'object' === $type,
+                sprintf("The Event of type %s is not type-compatible with type %s", get_class($event), $type)
+            );
+        }
+    }
+
     public function test_get_listeners_for_event_match()
     {
         $listeners = [
@@ -32,4 +82,12 @@ class DefaultListenerProviderTest extends TestCase
 
         $this->assertSame([], $listenerProvider->getListenersForEvent(new \stdClass));
     }
+}
+
+class A
+{
+}
+
+class B extends A
+{
 }
