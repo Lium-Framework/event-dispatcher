cd86bef9ff322e3e352067d3b1515947675cf6b6
hermann8u <florian.hermann94@gmail.com>
2019-12-09 12:53:30 +0100

    Add doc block on the interface

--- src/ListenerProvider/ResettableListenerProviderInterface.php
+++ src/ListenerProvider/ResettableListenerProviderInterface.php
@@ -1,9 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Lium\EventDispatcher\ListenerProvider;
 
 use Psr\EventDispatcher\ListenerProviderInterface;
 
+/**
+ * Provides a way to reset a listener provider to its initial state.
+ *
+ * When calling the "reset()" method on a listener provider, it should be put back
+ * to its initial state. This usually means clearing any internal buffers and forwarding
+ * the call to internal dependencies. All properties of the object should be put
+ * back to the same state it had when it was first ready to use.
+ */
 interface ResettableListenerProviderInterface extends ListenerProviderInterface
 {
     public function reset(): void;
