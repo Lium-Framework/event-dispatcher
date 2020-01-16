0df8b3acebd9f74429067a00febb4b0dd7fad0e4
hermann8u <florian.hermann94@gmail.com>
2020-01-16 23:21:56 +0100

    Conitnue refactor with phpinsight

--- src/ListenerProvider/DefaultListenerProvider.php
+++ src/ListenerProvider/DefaultListenerProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Lium\EventDispatcher\ListenerProvider;
 
-use Lium\EventDispatcher\Exception\InvalidListener;
 use Psr\EventDispatcher\ListenerProviderInterface;
 
 /**
@@ -13,11 +12,7 @@ use Psr\EventDispatcher\ListenerProviderInterface;
  */
 final class DefaultListenerProvider implements ListenerProviderInterface
 {
-    /** @var array<callable> */
-    private $listeners;
-
-    /** @var array<string>|null */
-    private $listenerArgumentMap;
+    use DefaultListenerProviderBehavior;
 
     /**
      * @param iterable<callable> $listeners
@@ -31,60 +26,5 @@ final class DefaultListenerProvider implements ListenerProviderInterface
         }
 
         $this->listeners = $listeners;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getListenersForEvent(object $event): iterable
-    {
-        if ($this->listenerArgumentMap === null) {
-            $this->prepareListenerParameterMap();
-        }
-
-        $listenersForEvent = [];
-        foreach ($this->listeners as $key => $listener) {
-            $listenerArgumentType = $this->listenerArgumentMap[$key];
-
-            if ($event instanceof $listenerArgumentType || $listenerArgumentType === 'object') {
-                $listenersForEvent[] = $listener;
-            }
-        }
-
-        return $listenersForEvent;
-    }
-
-    /**
-     * Prepare a map between a listener key and the listener first parameter class name thanks to \ReflectionFunction.
-     * If the listener has no parameter or if this parameter is not type-hinted with a class name or with "object", the
-     * listener is removed from the stack.
-     *
-     * @throws \ReflectionException
-     */
-    private function prepareListenerParameterMap(): void
-    {
-        $this->listenerArgumentMap = [];
-
-        foreach ($this->listeners as $key => $listener) {
-            $closure = \Closure::fromCallable($listener);
-            $reflectionFunction = new \ReflectionFunction($closure);
-
-            $reflectionParameter = $reflectionFunction->getParameters()[0] ?? null;
-            if ($reflectionParameter === null) {
-                throw new InvalidListener($listener, 'The listener must have one argument corresponding to the event it listen.');
-            }
-
-            $type = $reflectionParameter->getType();
-            if ($type === null) {
-                throw new InvalidListener($listener, 'The listener argument must have a type corresponding to the event it listen.');
-            }
-
-            $typeName = $type->getName();
-            if ($typeName !== 'object' && $reflectionParameter->getClass() !== null) {
-                throw new InvalidListener($listener, 'The listener argument must have the type of the event it listen or the scalar type "object".');
-            }
-
-            $this->listenerArgumentMap[$key] = $typeName;
-        }
     }
 }
---
+++ src/ListenerProvider/DefaultListenerProviderBehavior.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lium\EventDispatcher\ListenerProvider;
+
+use Lium\EventDispatcher\Exception\InvalidListener;
+
+trait DefaultListenerProviderBehavior
+{
+    /** @var array<callable> */
+    private $listeners;
+
+    /** @var array<string>|null */
+    private $listenerArgumentMap;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getListenersForEvent(object $event): iterable
+    {
+        if ($this->listenerArgumentMap === null) {
+            $this->listenerArgumentMap = [];
+            foreach ($this->listeners as $key => $listener) {
+                $this->listenerArgumentMap[$key] = $this->getListenerUniqueParameterType($listener);
+            }
+        }
+
+        $listenersForEvent = [];
+        foreach ($this->listeners as $key => $listener) {
+            $listenerArgumentType = $this->listenerArgumentMap[$key];
+
+            if ($event instanceof $listenerArgumentType || $listenerArgumentType === 'object') {
+                $listenersForEvent[] = $listener;
+            }
+        }
+
+        return $listenersForEvent;
+    }
+
+    /**
+     * Prepare a map between a listener key and the listener first parameter class name thanks to \ReflectionFunction.
+     * If the listener has no parameter or if this parameter is not type-hinted with a class name or with "object", the
+     * listener is removed from the stack.
+     *
+     * @param callable $listener
+     *
+     * @return string
+     * @throws \ReflectionException
+     */
+    private function getListenerUniqueParameterType(callable $listener): string
+    {
+        $closure = \Closure::fromCallable($listener);
+        $reflectionFunction = new \ReflectionFunction($closure);
+
+        $reflectionParameter = $reflectionFunction->getParameters()[0] ?? null;
+        if ($reflectionParameter === null) {
+            throw new InvalidListener($listener, 'The listener must have one argument corresponding to the event it listen.');
+        }
+
+        $type = $reflectionParameter->getType();
+        if ($type === null) {
+            throw new InvalidListener($listener, 'The listener argument must have a type corresponding to the event it listen.');
+        }
+
+        $typeName = $type->getName();
+        if ($typeName !== 'object' && $reflectionParameter->getClass() !== null) {
+            throw new InvalidListener($listener, 'The listener argument must have the type of the event it listen or the scalar type "object".');
+        }
+
+        return $typeName;
+    }
+}
