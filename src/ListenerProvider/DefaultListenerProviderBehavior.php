b59abb21ac2cd58388c865d739d42c0dee9dd9a8
hermann8u <florian.hermann94@gmail.com>
2020-01-21 14:21:31 +0100

    WIP

--- src/ListenerProvider/DefaultListenerProviderBehavior.php
+++ src/ListenerProvider/DefaultListenerProviderBehavior.php
@@ -4,54 +4,36 @@ declare(strict_types=1);
 
 namespace Lium\EventDispatcher\ListenerProvider;
 
-use Lium\EventDispatcher\Exception\InvalidListener;
+use Lium\EventDispatcher\Listener\Listener;
 
 trait DefaultListenerProviderBehavior
 {
     /** @var array<callable> */
     private $listeners;
 
-    /** @var array<string>|null */
-    private $listenerParameterMap;
+    /** @var array<Listener>|null */
+    private $listenersObjects;
 
     /**
      * {@inheritdoc}
      */
     public function getListenersForEvent(object $event): iterable
     {
-        if ($this->listenerParameterMap === null) {
+        if ($this->listenersObjects === null) {
             // Prepare the map
-            $this->listenerParameterMap = array_map(
-                [$this, 'getListenerUniqueParameterType'],
+            $this->listenersObjects = array_map(
+                [$this, 'initListener'],
                 $this->listeners
             );
         }
 
-        return array_filter($this->listeners, function (int $key) use ($event) {
-            return $event instanceof $this->listenerParameterMap[$key] || $this->listenerParameterMap[$key] === 'object';
-        }, ARRAY_FILTER_USE_KEY);
+        return array_filter($this->listeners, static function (callable $listener) use ($event) {
+            return $listener->matchEvent($event);
+        });
     }
 
-    private function getListenerUniqueParameterType(callable $listener): string
+    private function initListener(callable $listener): Listener
     {
-        $closure = \Closure::fromCallable($listener);
-        $reflectionFunction = new \ReflectionFunction($closure);
-
-        $reflectionParameter = $reflectionFunction->getParameters()[0] ?? null;
-        if ($reflectionParameter === null) {
-            throw new InvalidListener($listener);
-        }
-
-        $type = $reflectionParameter->getType();
-        if ($type === null) {
-            throw new InvalidListener($listener);
-        }
-
-        $typeName = $type->getName();
-        if ($typeName !== 'object' && $reflectionParameter->getClass() === null) {
-            throw new InvalidListener($listener);
-        }
-
-        return $typeName;
+        return new Listener($listener);
     }
 }
